e9ccb20001f14ad13dee2d908c5f7c9779e4b815
ArindamDutta02082001 <83761396+ArindamDutta02082001@users.noreply.github.com>
2024-01-06 21:05:08 +0530

    changed folder name to commonComponent

--- mini-react-router/src/routerPaths/Layout.js
+++ mini-react-router/src/routerPaths/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../componentToLearn/Header";
-import Footer from "../componentToLearn/Footer";
+import Header from "../commonComponent/Header";
+import Footer from "../commonComponent/Footer"
 import { Outlet } from "react-router-dom";
 
 // the layout is created to provide a prototype ,
--- mini-react-router/src/routerPaths/RouterPaths.js
+++ mini-react-router/src/routerPaths/RouterPaths.js
@@ -9,10 +9,11 @@ import Contact from "../staticComponent/Contact";
 import About from "../staticComponent/About";
 
 import Layout from "./Layout";
-import User from "../componentToLearn/User";
+import User from "../commonComponent/User";
 
 //this router path is created here in seperate file just to make look index.js cleaner
 // the layout is created to provide a prototype , and the outlet part will be replaced by home , about , contact materials as per required
+
 // method 1 :
 const routerPaths1 = createBrowserRouter([
   {
@@ -45,3 +46,68 @@ function RouterPath() {
 }
 
 export default RouterPath;
+
+
+
+// method 2 - in index.js file only
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <Switch>
+//         <Route exact path="/login">
+//           <Login />
+//         </Route>
+//         <Route exact path="/loginseller">
+//           <LoginSeller />
+//         </Route>
+
+//         <Route exact path="/">
+//           <Header />
+//           <DropdownAdmin />
+//           <Banner />
+//           <BodyProduct />
+//           <Footer />
+//         </Route>
+//         <Route exact path="/checkout">
+//           <Header />
+//           <Checkout />
+//           <Footer />
+//         </Route>
+//         <Route exact path="/payment">
+//           <Header />
+//           <Elements stripe={promise}>
+//             <Pay />
+//           </Elements>
+//           <Footer />
+//         </Route>
+//         <Route exact path="/thanks">
+//           <Header />
+//           <Thank />
+//           <Footer />
+//         </Route>
+//         <Route exact path="/delivery">
+//           <Header />
+//           <Delivery />
+//           <Footer />
+//         </Route>
+//         <Route exact path="/admin">
+//           <HeaderSeller />
+//           <Admin />
+//           <Footer />
+//         </Route>
+//         <Route exact path="/Thankyou">
+//           <Header />
+//           <Thankyou />
+//           <Footer />
+//         </Route>
+//         <Route exact path="/returnandorder">
+//           <Header />
+//           <Return />
+//           <Footer />
+//         </Route>
+//       </Switch>
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
